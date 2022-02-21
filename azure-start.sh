38042614a6642b8247ad688cb5e964babfd0761b
Well5a <marcelfrederik@aol.de>
2022-02-21 11:19:47 +0100

    improve azure-start.sh

--- azure-start.sh
+++ azure-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# azure login
+# Azure login
+az account show &>/dev/null || az login
 
 # Setup Kubernetes Cluster
 terraform -chdir=terraform/ init
@@ -10,6 +11,7 @@ terraform -chdir=terraform/ apply -auto-approve
 az aks get-credentials --resource-group t2store-resources --name t2store-aks1
 
 # Install t2-store
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install mongo-cart --set auth.enabled=false bitnami/mongodb
 helm install mongo-order --set auth.enabled=false bitnami/mongodb
 helm install kafka bitnami/kafka
