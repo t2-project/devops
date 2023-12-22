d3b51cc39a30c6a3f723941eda8c62740d49d396
David Kopp <git@davidkopp.de>
2023-12-22 15:47:16 +0100

    Improve build script

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,7 @@
 CLEAN_BUILD=false
 SKIP_TESTS=true
 DOCKER_TAG=1.3
-DOCKER_PUSH=true
+DOCKER_PUSH=false
 
 echo -e "Building T2-Project\n-------------------"
 
@@ -17,7 +17,7 @@ source $T2_DIR/devops/setenv.sh
 declare -a projects=("common" "cart" "creditinstitute" "inventory" "orchestrator" "order" "payment" "ui" "uibackend" "e2e-tests")
 for project in "${projects[@]}"
 do
-    echo -e "\nCompile '$project'"
+    echo -e "\nBuild & Install '$project'\n"
     cd $T2_DIR/$project
     if [ "$CLEAN_BUILD" = true ] ; then
         ./mvnw clean
@@ -28,6 +28,11 @@ do
         ./mvnw install -DskipTests
     fi
 
+    # Install dependencies of E2E Test to local maven repository
+    if [ "$project" == 'payment' ] || [ "$project" == 'inventory' ] || [ "$project" == 'order' ] ; then
+        ./mvnw install:install-file -Dfile=./target/$project-0.0.1-SNAPSHOT.jar.original
+    fi
+
     # Docker build everything, except 'common' and 'e2e-tests'
     if [ "$project" != 'common' ] && [ "$project" != 'e2e-tests' ] ; then
         docker build -t t2project/$project:$DOCKER_TAG .
@@ -35,6 +40,7 @@ do
             docker push t2project/$project:$DOCKER_TAG
         fi
     fi
+
     # Build e2e-tests with different name
     if [ "$project" = 'e2e-tests' ] ; then
         docker build -t t2project/e2etest:$DOCKER_TAG .
