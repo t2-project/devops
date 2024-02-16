e4a5230aba3f19dea910eb4e6416898398015a25
David Kopp <git@davidkopp.de>
2024-02-16 10:30:05 +0100

    Improve tf prometheus module

--- terraform/modules/prometheus/prometheus.tf
+++ terraform/modules/prometheus/prometheus.tf
@@ -1,37 +1,17 @@
-/* resource "helm_release" "prometheus" {
-  name       = "prometheus"
-  chart      = "kube-prometheus-stack"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  namespace  = var.namespace
-  count = var.create_module ? 1 : 0
-
-  set {
-    name = "alertmanager.persistentVolume"
-    value = "false"
-  }
-
-  set {
-    name = "server.persistentVolume.enabled"
-    value = "false"
-  }
-  
-} */
-
 resource "helm_release" "prometheus" {
-  name             = "kube-prometheus-stackr"
+  name             = "prometheus"
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
   namespace        = var.namespace
   create_namespace = true
-  version          = "45.7.1"
+  version          = "56.6.2"
   count            = var.create_module ? 1 : 0
 
-
   values = [
-    "${file("./configs/values.yaml")}"
+    # "${file("./configs/prometheus-values.yaml")}"
+    "${file("${abspath(path.module)}/values.yaml")}"
   ]
   timeout = 2000
-
 
   set {
     name  = "podSecurityPolicy.enabled"
--- terraform/modules/prometheus/variables.tf
+++ terraform/modules/prometheus/variables.tf
@@ -1,6 +1,6 @@
 variable "namespace" {
   type    = string
-  default = "prometheus"
+  default = "monitoring"
 }
 
 variable "create_module" {
