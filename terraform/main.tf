bea67909ad7e7704294cc399d0a017f5d7b43d6c
David Kopp <git@davidkopp.de>
2024-02-18 17:50:48 +0100

    Re-add modules directory

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,19 @@
 # module "t2project" {
-#   source        = "./t2project"
+#   source        = "./modules/t2project"
 #   namespace     = var.app-namespace
 #   create_module = true
 #   depends_on    = [kubernetes_namespace.app-namespace, module.eks, module.kind]
 # }
 
 module "prometheus" {
-  source        = "./prometheus"
+  source        = "./modules/prometheus"
   create_module = var.create_prometheus
   namespace     = var.measurement-namespace
   depends_on    = [kubernetes_namespace.measurement-namespace, module.eks, module.kind]
 }
 
 module "kepler" {
-  source        = "./kepler"
+  source        = "./modules/kepler"
   create_module = var.create_kepler
   namespace     = var.measurement-namespace
   // set to true on some system, not sure if it works as intended
@@ -22,7 +22,7 @@ module "kepler" {
 }
 
 module "kind" {
-  source        = "./kind"
+  source        = "./modules/kind"
   create_module = var.create_kind
   set_kubecfg   = true
   kube_config   = var.kube_config
@@ -33,7 +33,7 @@ module "kind" {
 # Note that the following module expects an awscli properly configured to an AWS account.
 # It takes about 10-20 minutes to start.
 module "eks" {
-  source        = "./eks"
+  source        = "./modules/eks"
   create_module = var.create_aws_eks
   aws_region    = "eu-north-1"
   set_kubecfg   = true
@@ -41,7 +41,7 @@ module "eks" {
 }
 
 module "azure" {
-  source        = "./azure"
+  source        = "./modules/azure"
   create_module = var.create_azure_aks
   set_kubecfg   = true
 }
