1388b1c9856e4858f4c0b053d45ceaab565749b4
David Kopp <git@davidkopp.de>
2024-02-24 10:59:58 +0100

    Update aws-ebs-csi-driver version

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -141,6 +141,7 @@ data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
+# https://registry.terraform.io/modules/terraform-aws-modules/iam/aws/latest
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "~> 5.34"
@@ -152,10 +153,12 @@ module "irsa-ebs-csi" {
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_addon
+# https://github.com/kubernetes-sigs/aws-ebs-csi-driver
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.27.0-eksbuild.1"
+  addon_version            = "v1.28.0-eksbuild.1"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
 
   tags = {
