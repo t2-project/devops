6b7a2a99169857c55551d6cf68c910bcbe17be59
David Kopp <git@davidkopp.de>
2024-02-29 22:27:21 +0100

    Add UIBackend LoadBalancer

--- terraform/aws-start.sh
+++ terraform/aws-start.sh
@@ -24,11 +24,17 @@ terraform -chdir=./environments/aws/ apply -auto-approve
 # Install T2-Project
 source $K8S_DIR/start.sh
 
-# Expose Grafana and wait for the hostname
+# Expose Grafana and UIBackend and wait for the hostnames
+# We expose Grafana because there are issues with Grafana and port-forward.
+# We expose UIBackend because port-forward does no load-balancing (it just picks one pod).
 kubectl apply -f $K8S_DIR/load-balancer/aws-loadbalancer-grafana.yaml
+kubectl apply -f $K8S_DIR/load-balancer/aws-loadbalancer-uibackend.yaml
 until kubectl get service/prometheus-grafana-nlb -n monitoring --output=jsonpath='{.status.loadBalancer}' | grep "ingress"; do : ; done
 GRAFANA_HOSTNAME=$(kubectl get service/prometheus-grafana-nlb -n monitoring -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 echo -e "\nGrafana URL: http://${GRAFANA_HOSTNAME}"
+until kubectl get service/uibackend-nlb -n default --output=jsonpath='{.status.loadBalancer}' | grep "ingress"; do : ; done
+UIBACKEND_HOSTNAME=$(kubectl get service/uibackend-nlb -n default -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+echo -e "\UIBackend URL: http://${UIBACKEND_HOSTNAME}"
 
 # Enable autoscaling (requires that the prometheus-adapter is enabled)
 # Wait until every backend app is ready to avoid unnecessary scaling during boot
