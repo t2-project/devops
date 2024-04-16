cc6c2527177e15b829bc0c51943c75461d476f6a
David Kopp <git@davidkopp.de>
2024-04-16 20:49:42 +0200

    Use less nodes, but bigger ones

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -58,14 +58,14 @@ module "eks" {
     node-group-1 = {
       platform       = "bottlerocket"
       ami_type       = "BOTTLEROCKET_x86_64"
-      instance_types = ["c5.large"]
+      instance_types = ["c5.xlarge"]
 
       # Not required nor used - avoid tagging two security groups with same tag as well
       create_security_group = false
 
       min_size     = 1
       max_size     = 10
-      desired_size = 6
+      desired_size = 3
 
       block_device_mappings = {
         xvdb = {
